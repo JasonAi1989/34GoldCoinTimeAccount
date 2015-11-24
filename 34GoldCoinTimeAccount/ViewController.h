742f5dfdcd931de1ad27a63e3f79047a655f4d27
jason <aizhaoyu@163.com>
2015-11-24 10:28:58 +0800

    Initial Commit

---
+++ 34GoldCoinTimeAccount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  34GoldCoinTimeAccount
+//
+//  Created by jason on 15/11/24.
+//  Copyright © 2015年 JasonAi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
